4d977361818bcc08f199824be6b37ce9ecf8bffa
krissasantos <krissa.santos31@gmail.com>
2016-10-24 22:22:29 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,14 +5,25 @@ class ProductsController < ApplicationController
     # render 'index'
   end
 
-
   def add
-    # @item = Item.find(params)
-    # cart << @product
     cart << params[:product]
     redirect_to '/'
   end
 
-
-
+  # def add
+  #   # @item = Item.find(params)
+  #   # cart << @product
+  #   cart << params[:product]
+  #   redirect_to '/'
+  # end
 end
+
+# <%= form_tag "/products/add" do %>
+# <%= text_field_tag  :product %>
+# <%= submit_tag "add to cart" %>
+# <% end %>
+
+
+# <% @cart.each do |thing| %>
+# <%=  thing %>
+# <%end%>
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # resources :products, only: [:index, :add, :cart]
 
   get "/" => "products#index"
-  post "/products/add" => "products#add"
+  post "/products" => "products#add"
   
 end
